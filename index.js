9a11a7efc2ee2ba97ef699e5efdb89e73f31cacc
MahmoudRumaneh <mahmoudrmn@gmail.com>
2023-10-16 16:29:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
+/*
+Pseudocode:
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+Create a function called hasTargetSum that takes an array and a target as parameters.
+Use a nested loop to iterate through all pairs of numbers in the array.
+The outer loop iterates from the first element to the second-to-last element.
+The inner loop iterates from the next element to the last element, avoiding duplicate pairs.
+Check if the sum of the current pair of numbers equals the target.
+If a pair is found with a sum equal to the target, return true.
+If no such pair is found after iterating through the entire array, return false.
 
-/* 
-  Add your pseudocode here
 */
 
 /*
-  Add written explanation of your solution here
-*/
+Solution Explanation:
 
-// You can run `node index.js` to view these console logs
+The `hasTargetSum` function checks if there exists a pair of numbers in the array that adds up to the target.
+ It returns `true` if such a pair is found; otherwise, it returns `false`.
+ This approach provides a basic solution before optimizing for efficiency.
+*/
 if (require.main === module) {
-  // add your own custom tests in here
+  // Test cases
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
